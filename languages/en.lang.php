124e1925dc2f63a67684ba498fdac754f95f72a2
Afonso Oliveira <af.oliveira.16@gmail.com>
2014-02-14 22:59:08 +0000

    add english language with some translations

---
+++ languages/en.lang.php
@@ -0,0 +1,15 @@
+<?php
+    return array(
+        'owlcarousel' => array(
+            'Owl Carousel' => 'Owl Caroucel',
+            'Create New Item' => 'Create New Item',
+            'Items' => 'Items',
+            'Owl Carousel plugin for Monstra' => 'Owl Carousel plugin for Monstra',
+            'Required field' => 'Required field',
+            'This item already exists' => 'This item already exists',
+            'The item has been added.' => 'The item has been added.',
+            'The item has been edited.' => 'The item has been edited.'
+        )
+    );
+
+?>
