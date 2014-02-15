842acd44d99c6414c6d3e2c27b1ea7f9e45f8ec8
Afonso Oliveira <af.oliveira.16@gmail.com>
2014-02-15 19:57:33 +0000

    add plugin functionamity

--- owlcarousel.plugin.php
+++ owlcarousel.plugin.php
@@ -15,8 +15,8 @@ if (Session::exists('user_role') && in_array(Session::get('user_role'), array('a
 }
 
 //Add Shortcodes
-
-
+Shortcode::add('owlcaroucel', 'OwlCarousel::_renderItems');
+Shortcode::add('owlcaroucelinit', 'OwlCarousel::_renderScript');
 
 //Add JS
 Javascript::add('plugins/owlcarousel/assets/js/owl.carousel.js');
@@ -28,16 +28,54 @@ Stylesheet::add('plugins/owlcarousel/assets/css/owl.transitions.css', 'frontend'
 
 class OwlCarousel
 {
-    public static $items = array();
+    public static $class_options = array();
 
-    public static function _renderItems()
+    public static function _renderItems($attributes, $content)
     {
-        
+        $class = "owl-carousel";
+        if(isset($attributes['class'])) $class = $attributes['class']; 
+        $html = "<div class=\"".$class."\">";
+        $owlcarousel_path = STORAGE.DS.'owlcarousel'.DS;
+        $item_hash = array();
+        if(isset($attributes['group'])){
+            $groups = array($attributes['group']);
+        }else{
+            $groups = OwlCarousel::getGroups();
+        }
+        foreach($groups as $group){
+            $item_hash[$group] = File::scan($owlcarousel_path.$group,'.owlitem.html');
+        }
+        foreach($item_hash as $group=>$items){
+            foreach($items as $item){
+                $html = $html."\n<div>".File::getContent($owlcarousel_path.$group.DS.$item)."</div>\n";
+            }
+        }
+        OwlCarousel::$class_options[$class] = $content;
+        $html = $html."</div>";
+        return Filter::apply('content',$html);
     }
 
-    public static function _renderScript()
+    public static function _renderScript($attributes, $content)
     {
-        
+       $class = "owl-carousel";
+       if(isset($attributes['class'])) $class = $attributes['class'];
+       if(isset(OwlCarousel::$class_options[$class])){
+           $html = "<script>$(\".".$class."\").owlCarousel(".OwlCarousel::$class_options[$class].");</script>";
+       }else{
+           $html = "";
+       }
+       return $html;
+    }
+
+    public static function getGroups()
+    {
+        $group_list = array();
+        $owlcarousel_path = STORAGE . DS . 'owlcarousel' . DS;
+        $dir = opendir($owlcarousel_path);
+        while(false !== ($d = readdir($dir))){
+            if($d != '.' and $d != '..' and is_dir($owlcarousel_path.$d)) $group_list[] = $d;
+        }
+        return $group_list;
     }
 }
 
